cc16b553539a79ea442d1d1604c1f1fc6eb7e00e
Antonio Tropiano <antoniotropiano@gmail.com>
2016-09-08 17:23:49 +0100

    minor fixes to visualisation

--- js/serie-a/scatter.js
+++ js/serie-a/scatter.js
@@ -63,7 +63,7 @@ d3.csv("teamdata.csv", function(error, data){
 		  .on("mouseover", function(d) {		
             div.transition()		
                 .duration(200)		
-                .style("opacity", .9);		
+                .style("opacity", .8);		
             div.html(d.region)	
                 .style("left", (d3.event.pageX) + "px")		
                 .style("top", (d3.event.pageY - 28) + "px");	
