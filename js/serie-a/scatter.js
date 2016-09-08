797f58dee30928d7b00c131ff60521511ee8d086
Antonio Tropiano <atropiano@decibelinsight.com>
2016-09-08 16:43:35 +0100

    fixed the visulaisation

--- js/serie-a/scatter.js
+++ js/serie-a/scatter.js
@@ -1,18 +1,20 @@
 //var data = [[5,3], [10,17], [15,4], [2,8]];
 d3.csv("teamdata.csv", function(error, data){
    
-    var margin = {top: 20, right: 15, bottom: 60, left: 60}
-      , width = 1000 - margin.left - margin.right
-      , height = 500 - margin.top - margin.bottom;
+    var margin = {top: 50, right: 50, bottom: 50, left: 50}
+      , width = 600 - margin.left - margin.right
+      , height = 600 - margin.top - margin.bottom;
     
     var x = d3.scale.linear()
-              .domain([0, d3.max(data, function(d) { return d["iv"]; })])
+              .domain([0, d3.max(data, function(d) { return +d["iii"]; })])
               .range([0, width ]);
     
     var y = d3.scale.linear()
-    	      .domain([0, d3.max(data, function(d) { return d["total"]; })])
+    	      .domain([0, d3.max(data, function(d) { return +d["total"]; })])
     	      .range([height, 0 ]);
  
+	
+	
     var chart = d3.select('body')
 	.append('svg:svg')
 	.attr('width', width + margin.right + margin.left)
@@ -50,7 +52,7 @@ d3.csv("teamdata.csv", function(error, data){
     g.selectAll("scatter-dots")
       .data(data)
       .enter().append("svg:circle")
-          .attr("cx", function (d,i) { return x(d["iv"]); } )
-          .attr("cy", function (d) { return y(d["total"]); } )
+          .attr("cx", function (d,i) { return x(+d["iii"]); } )
+          .attr("cy", function (d) { return y(+d["total"]); } )
           .attr("r", 5);
 });
